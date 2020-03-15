f7284728e2257952afba286817367f46298cc991
Juindz <47649121+Juindz@users.noreply.github.com>
2020-03-14 22:55:33 -0700

    Update visualization.py

--- FinalProject/SentimentAnalysis/visualization.py
+++ FinalProject/SentimentAnalysis/visualization.py
@@ -4,10 +4,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-external_stylesheets = [
+external_stylesheets1 = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css',
     {
         'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
@@ -16,21 +17,13 @@ external_stylesheets = [
         'crossorigin': 'anonymous'
     }
 ]
-# upper overview 91 143 186
 
-# value=dict(rgb=dict(r=255, g=0, b=0, a=0))
-# anger 234 118 142
-# surprise 124 190 209
-# anticipation 242 268 103
-# trust 184 216 121
-# fear 140 215 159
-# joy 250 224 119
-# surprise 126 190 207
-# disgust 171 142 192
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # lower counts 182 205 224
 # lower hover 109 256 194
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# external_stylesheets=external_stylesheets
+app = dash.Dash(__name__)
 app.layout = html.Div([
     html.H1("PEARL by Zijun Deng and Nuoshi Li", style={"textAlign": "center"}),
     html.Div([
@@ -41,57 +34,71 @@ app.layout = html.Div([
                 options=[
                     {'label': '14874721', 'value': 'pulvereyes'},
                     {'label': '14396017', 'value': 'wkulhanek'},
-                    {'label': 'Compare tweets from two parties', 'value': 'politics'}
+                    {'label': 'Democrats', 'value': 'TheDemocrats'},
+                    {'label': 'Republicans', 'value': 'GOP'}
                 ],
                 value='pulvereyes',
-                style=({"textAlign": "center", "width": "50%", "left": 20})
+                style=({"textAlign": "center", "width": "50%", "left": 20, 'font-family': 'Open Sans'})
             )
-        ]),
+        ],
+            style=({"width": "49%", 'display': 'inline-block', 'border-radios': '10px'})),
         html.Div([
             dcc.Checklist(
                 id="emotionCheckList",
                 options=[
-                    {'label': 'Anger', 'value': 'anger'},
-                    {'label': 'Disgust', 'value': 'disgust'},
-                    {'label': 'Joy', 'value': 'joy'},
-                    {'label': 'Surprise', 'value': 'surprise'},
-                    {'label': 'Anticipation', 'value': 'anticipation'},
-                    {'label': 'Fear', 'value': 'fear'},
-                    {'label': 'Sadness', 'value': 'sadness'},
-                    {'label': 'Trust', 'value': 'trust'}
+                    {'label': 'anger', 'value': 'anger'},
+                    {'label': 'disgust', 'value': 'disgust'},
+                    {'label': 'joy', 'value': 'joy'},
+                    {'label': 'surprise', 'value': 'surprise'},
+                    {'label': 'anticipation', 'value': 'anticipation'},
+                    {'label': 'fear', 'value': 'fear'},
+                    {'label': 'sadness', 'value': 'sadness'},
+                    {'label': 'trust', 'value': 'trust'}
                 ],
                 value=['anger', 'fear', 'anticipation', 'surprise', 'joy', 'sadness', 'trust', 'disgust'],
-                labelStyle={'display': 'inline-block', "right": 50, "font": 50}
+                labelStyle={'display': 'inline-block'}
             )
-        ])
-    ]),
+        ],
+            id='radioitems',
+            style={'width': '49%', 'float': 'right', 'display': 'inline-block', 'border-radios': '8px',
+                   'font-family': 'Open Sans', 'font-size': '14'})
+    ],
+        style={
+            'borderBottom': 'thin lightgrey solid',
+            'backgroundColor': 'rgb(205, 205, 205)',
+            'border-radius': '10px',
+            'border-color': 'rgb(170, 170, 170)',
+            'border-width': '3px',
+            'padding': '10px 5px',
+            'right': '30px'
+        }
+    ),
     html.Div([
         html.Div([
-            #html.H1("overview", style={"textAlign": "center"}),
-            dcc.Graph(id='overview_vis',
-                      style={'height': 300})
+            # html.H1("overview", style={"textAlign": "center"}),
+            dcc.Graph(id='overview_vis')
         ]),
         html.Div([
-            #html.H1("expand view", style={"textAlign": "center"}),
-            dcc.Graph(id='expandview_vis', style={'height': 400}),
-        ]),
-        html.Div([
-            #html.H1("Mood view", style={"textAlign": "center"}),
-            dcc.Graph(id='moodview_vis', style={'height': 400}),
+            # html.H1("expand view", style={"textAlign": "center"}),
+            dcc.Graph(id='expandview_vis')
         ])
+        # html.Div([
+        # html.H1("Mood view", style={"textAlign": "center"}),
+        # dcc.Graph(id='moodview_vis')
+        # ])
     ]),
     # left: scatter plot of words within the same time frame
     # right: set of tweets within the same time frame
     html.Div([
         html.Div([
-            html.H1("VAD", style={"textAlign": "center"}),
+            html.H5("VAD", style={"textAlign": "center"}),
             dcc.Graph(id='vad_vis')
-        ], className="six columns"),
+        ], style={'width': '40%', 'display': 'inline-block', 'padding': '0 20'}),
         html.Div([
-            html.H1("tweets", style={"textAlign": "center"}),
+            html.H5("tweets", style={"textAlign": "center"}),
             dcc.Graph(id='tweets_vis')
-        ], className="six columns")
-    ], className="row")
+        ], style={'display': 'inline-block', 'width': '59%'})
+    ])
 ])
 
 
@@ -109,133 +116,194 @@ def update_overview(user):
     df['DateTime'] = pd.to_datetime(df['DateTime'])
     df.index = df['DateTime']
     dg = df.resample('D').count()
-    #print(dg)
-    fig.add_trace(go.Scatter(x=df['DateTime'], y=dg['Original Tweet'], fill='tozeroy', mode='none'))
+    # print(dg)
+    fig.add_trace(go.Scatter(
+        x=df['DateTime'],
+        y=dg['Original Tweet'],
+        fill='tozeroy',
+        mode='none',
+        name='Tweet Counts'))
     fig.update_layout(
+        height=200,
+        width=1363,
+        margin=dict(l=20, r=20, t=20, b=20),
         showlegend=True,
         yaxis_range=(0, 10)
+    )
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=2,
+                         label="2m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
     )
     return fig
 
 
 @app.callback(
     dash.dependencies.Output('expandview_vis', 'figure'),
-    [dash.dependencies.Input('userDropDown', 'value')]
+    [dash.dependencies.Input('userDropDown', 'value'),
+     dash.dependencies.Input('emotionCheckList', 'value')]
 )
-def update_expandview(user):
+def update_expandview(user, emotion):
     # on top of valance?
     file = 'new_output_' + user + '.csv'
     df = pd.read_csv(file)
     df.sort_values(by=['DateTime'])
-    emotions = []
-    # color based on colum name
-    # x = datetime
-    # y = 8 emotion columns
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['sadness'],
-        hovertext='sadness',
-        hoverinfo='text',
-        mode='lines',
-        name='sadness',
-        line=dict(width=0.5, color='rgb(234, 118, 142)'),
-        stackgroup='one'
-    ))
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['surprise'],
-        hovertext='surprise',
-        hoverinfo='text',
-        mode='lines',
-        name='surprise',
-        line=dict(width=0.5, color='rgb(140, 215, 159)'),
-        stackgroup='one'
-    ))
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['fear'],
-        hovertext='fear',
-        hoverinfo='text',
-        mode='lines',
-        name='fear',
-        line=dict(width=0.5, color='rgb(242, 268, 103)'),
-        stackgroup='one'
-    ))
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['trust'],
-        hovertext='trust',
-        hoverinfo='text',
-        mode='lines',
-        name='trust',
-        line=dict(width=0.5, color='rgb(124, 190, 209)'),
-        stackgroup='one'
-    ))
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['joy'],
-        hovertext='joy',
-        hoverinfo='text',
-        mode='lines',
-        name='joy',
-        line=dict(width=0.5, color='rgb(250, 224, 119)'),
-        stackgroup='one'
-    ))
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['anticipation'],
-        hovertext='anticipation',
-        hoverinfo='text',
-        mode='lines',
-        name='anticipation',
-        line=dict(width=0.5, color='rgb(184, 247, 212)'),
-        stackgroup='one'
-    ))
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['anger'],
-        hovertext='anger',
-        hoverinfo='text',
-        mode='lines',
-        name='anger',
-        line=dict(width=0.5, color='rgb(184, 216, 121)'),
-        stackgroup='one'
-    ))
-    fig.add_trace(go.Scatter(
-        x=df['DateTime'],
-        y=df['disgust'],
-        hovertext='disgust',
-        hoverinfo='text',
-        mode='lines',
-        name='disgust',
-        line=dict(width=0.5, color='rgb(171, 142, 192)'),
-        stackgroup='one'
-    ))
-    fig.update_layout(
-        showlegend=True,
-        yaxis_range=(-0.5, 1.5))
-    return fig
-
-
-
-@app.callback(
-    dash.dependencies.Output('moodview_vis', 'figure'),
-    [dash.dependencies.Input('userDropDown', 'value')]
-)
-def update_moodview(user):
-    # area = the amount of tweets
-    file = 'new_output_' + user + '.csv'
-    df = pd.read_csv(file)
-    df.sort_values(by=['DateTime'])
+    # print(emotion)
     # height by the number of tweets of the day
-    fig = go.Figure()
     df['DateTime'] = pd.to_datetime(df['DateTime'])
     df.index = df['DateTime']
-    fig.add_trace(go.Scatter(x=df['DateTime'], y=df['keyword_counts'], fill='tozeroy', mode='none'))
+    dg = df.resample('D').count()
+    # fig = go.Figure()
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    if 'sadness' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['sadness'],
+            hovertext='sadness',
+            hoverinfo='text',
+            mode='lines',
+            name='sadness',
+            line=dict(width=0.2, color='rgb(127, 156, 200)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    if 'surprise' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['surprise'],
+            hovertext='surprise',
+            hoverinfo='text',
+            mode='lines',
+            name='surprise',
+            line=dict(width=0.2, color='rgb(106, 164, 190)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    if 'fear' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['fear'],
+            hovertext='fear',
+            hoverinfo='text',
+            mode='lines',
+            name='fear',
+            line=dict(width=0.2, color='rgb(134, 185, 126)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    if 'trust' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['trust'],
+            hovertext='trust',
+            hoverinfo='text',
+            mode='lines',
+            name='trust',
+            line=dict(width=0.2, color='rgb(168, 200, 70)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    if 'joy' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['joy'],
+            hovertext='joy',
+            hoverinfo='text',
+            mode='lines',
+            name='joy',
+            line=dict(width=0.2, color='rgb(240, 218, 106)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    if 'anticipation' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['anticipation'],
+            hovertext='anticipation',
+            hoverinfo='text',
+            mode='lines',
+            name='anticipation',
+            line=dict(width=0.2, color='rgb(213, 159, 97)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    if 'anger' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['anger'],
+            hovertext='anger',
+            hoverinfo='text',
+            mode='lines',
+            name='anger',
+            line=dict(width=0.2, color='rgb(226, 53, 88)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    if 'disgust' in emotion:
+        fig.add_trace(go.Scatter(
+            x=df['DateTime'],
+            y=df['disgust'],
+            hovertext='disgust',
+            hoverinfo='text',
+            mode='lines',
+            name='disgust',
+            line=dict(width=0.2, color='rgb(165, 50, 189)'),
+            stackgroup='one'
+        ), secondary_y=False)
+    # print(dg)
+    fig.add_trace(go.Scatter(
+        x=df['DateTime'],
+        y=dg['Original Tweet'],
+        fill='tozeroy',
+        mode='lines',
+        name='Tweet Counts',
+        line=dict(width=0.2, color='rgb(182, 105, 224)')
+    ), secondary_y=True)
     fig.update_layout(
+        height=300,
+        margin=dict(l=20, r=0, t=20, b=20),
         showlegend=True,
-        yaxis_range=(0, 10)
+        yaxis_range=(-0.25, 1.25)
+    )
+    # add rannge slider
+    # reference: https://plot.ly/python/range-slider/
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=2,
+                         label="2m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
     )
     return fig
 
